ca0cdd8c1f7858097e55731b014b1df079c188e0
snopkowaty <61710433+snopkowaty@users.noreply.github.com>
2020-03-26 20:43:19 +0100

    Zadanie16-poprawki

--- src/Zadanie16.java
+++ src/Zadanie16.java
@@ -1,7 +1,8 @@
 public class Zadanie16 {
     public static void main(String[] args) {
         int sum = 0;
-        for(int x=-30;x<=1566;sum += x++) {
+        for(int x=-30;x<=1566;x++) {
+            sum += x;
         }
         System.out.println("Suma po loopie to " +sum);
     }
