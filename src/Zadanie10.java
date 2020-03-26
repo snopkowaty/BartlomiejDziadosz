e6c5e1422870dbdc53d76f388622a5815e35d7bf
snopkowaty <61710433+snopkowaty@users.noreply.github.com>
2020-03-26 13:16:33 +0100

    Update Zadanie10.java

--- src/Zadanie10.java
+++ src/Zadanie10.java
@@ -5,12 +5,7 @@ public class Zadanie10 {
 
         Scanner in = new Scanner(System.in);
         for (;;) {
-            System.out.println("Odgadnij liczbę");
-            double a = (Math.random()*100);
-            int a1 = (int) a;
-            double b = in.nextInt();
-            if (a1 == b) System.out.println("Zgadłeś");
-            else System.out.println("Nie zgadłeś, losowa liczba to " + a1);
+
 
         }
 
