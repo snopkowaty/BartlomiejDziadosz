8b5ff5c285568d996c0bbbc8a933a2e445f320c2
snopkowaty <61710433+snopkowaty@users.noreply.github.com>
2020-03-26 13:57:05 +0100

    Zadanie14-koniec

--- src/Zadanie14.java
+++ src/Zadanie14.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class Zadanie14 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        strin
+        System.out.println("Podaj liczbę +");
+        int x = in.nextInt(),counter=0;
+        do {
+            System.out.print("+");
+            counter++;
+        } while (counter != x);
     }
 }
