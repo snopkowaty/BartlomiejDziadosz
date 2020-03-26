69a124703e657d829baa020609baa8da2c9710c9
snopkowaty <61710433+snopkowaty@users.noreply.github.com>
2020-03-26 20:41:02 +0100

    Zadanie9-poprawki

--- src/Zadanie9.java
+++ src/Zadanie9.java
@@ -5,9 +5,10 @@ public class Zadanie9 {
         Scanner input = new Scanner(System.in);
         System.out.println("Podaj liczbę: ");
         int a = input.nextInt();
-        if (a!=0) {
-            if (a > 0) System.out.println("Liczba jest oddatnia");
-            else System.out.println("Liczba jest ujemna");
-        } else System.out.println("Liczba jest zerem");
+            if (a != 0) {
+                if (a > 0) {
+                    System.out.println("Liczba jest oddatnia");
+                } else System.out.println("Liczba jest ujemna");
+            } else System.out.println("Liczba jest zerem");
     }
 }
