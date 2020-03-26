0180e7163bc41c9c460461c70b1106738f12ac6c
snopkowaty <61710433+snopkowaty@users.noreply.github.com>
2020-03-26 20:10:48 +0100

    Zadanie19-koniec

---
+++ src/Zadanie19.java
@@ -0,0 +1,9 @@
+public class Zadanie19 {
+    public static void main(String[] args) {
+        for (int x = 51; x <=127; x++) {
+            if ((x%2==0) && (x%3==0)) System.out.println(x);
+
+        }
+
+    }
+}
